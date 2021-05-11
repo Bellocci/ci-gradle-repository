5d9458bcd69dce334064d6a27b37ddc182e74cc7
Bellocci Francesco <francesco.bellocci@stud.unifi.it>
2021-05-11 20:28:00 +0200

    Add profile-default.gradle and profile-jacoco.gradle

--- ci-gradle/src/main/java/com/examples/myproject/App.java
+++ ci-gradle/src/main/java/com/examples/myproject/App.java
@@ -5,4 +5,11 @@ public class App {
 	public String sayHello() {
 		return "Hello";
 	}
+	
+	public String sayHelloWorld() {
+		String hello = sayHello();
+		String world = "World";
+		System.out.println(hello + " " + world);
+		return hello + world;
+	}
 }
