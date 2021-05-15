4a79625f6c13e3ac50a971ec4897801102b41ed6
Bellocci Francesco <francesco.bellocci@stud.unifi.it>
2021-05-15 16:23:10 +0200

    Try to add docker, create image, create start and stop container

---
+++ ci-gradle/src/main/java/com/examples/myproject/main/Main.java
@@ -0,0 +1,14 @@
+package com.examples.myproject.main;
+
+import com.examples.myproject.App;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		App app = new App();
+		System.out.println(app.sayHello("world"));
+
+	}
+
+}
